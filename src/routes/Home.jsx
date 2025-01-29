4b151feca90b31b64afd66ca2a5345c43a4d18fc
Hsoren31 <sorensenhaley@gmail.com>
2025-01-29 15:46:48 -0700

    Style: Add/style content to home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/Home.module.css";
+
 function Home() {
   return (
-    <div>
-      <p>Say something here. I am on home page...</p>
+    <div id="welcome-page">
+      <h2 className={styles.header}>Welcome to Shop Store Fake!</h2>
+      <div className={styles.shopAll}>
+        <button>Shop All</button>
+      </div>
+      <section className={styles.cardsFlex}>
+        <div className={styles.womens}>
+          <button>Shop All</button>
+        </div>
+        <div className={styles.mens}>
+          <button>Shop Men's</button>
+        </div>
+        <div className={styles.electronics}>
+          <button>Shop Electronic's</button>
+        </div>
+        <div className={styles.jewelry}>
+          <button>Shop Jewelry</button>
+        </div>
+      </section>
     </div>
   );
 }
