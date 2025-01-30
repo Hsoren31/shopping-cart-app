44666ccf8b039a2e7fa38a3af1b07363b5452474
Hsoren31 <sorensenhaley@gmail.com>
2025-01-30 14:12:06 -0700

    Style: tell user cart is empty & link to shop page

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/Cart.module.css";
+
 function Cart() {
   return (
-    <div>
-      <p>Now Showing Cart Page.</p>
+    <div className={styles.empty}>
+      <p className={styles.emptyText}>Looks like your cart is empty!</p>
+      <br></br>
+      <Link className={styles.shopLink} to="/shop">
+        Start Shopping
+      </Link>
     </div>
   );
 }
